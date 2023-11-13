b40fee4f997640b240c5584fc3bc57d21fb1bf3d
Lirae Que Data <liraedata59@gmail.com>
2023-11-13 16:04:30 +0800

    Add new methods

--- backend/programs/backend/src/lib.rs
+++ backend/programs/backend/src/lib.rs
@@ -1,4 +1,24 @@
+use anchor_lang::prelude::*;
 
+pub mod utils {
+    pub mod constant;
+    pub mod state;
+}
 
+use crate::utils::*;
+use crate::utils::*;
 
 declare_id!("92TRmey1biFtJMR8oMCMd2oZQmfiAF5CR3zvTQPixLCp");
+
+#[program]
+pub mod main {
+    use super::*;
+
+    pub fn init_user(){
+
+    }
+
+    pub fn create_post(){
+        
+    }
+}
--- backend/target/debug/deps/backend-4fc3e37e664c60a9.d
+++ backend/target/debug/deps/backend-4fc3e37e664c60a9.d
@@ -1,5 +1,7 @@
-/home/data/Desktop/project/Outburst/backend/target/debug/deps/backend-4fc3e37e664c60a9.rmeta: programs/backend/src/lib.rs
+/home/data/Desktop/project/Outburst/backend/target/debug/deps/backend-4fc3e37e664c60a9.rmeta: programs/backend/src/lib.rs programs/backend/src/utils/constant.rs programs/backend/src/utils/state.rs
 
-/home/data/Desktop/project/Outburst/backend/target/debug/deps/backend-4fc3e37e664c60a9.d: programs/backend/src/lib.rs
+/home/data/Desktop/project/Outburst/backend/target/debug/deps/backend-4fc3e37e664c60a9.d: programs/backend/src/lib.rs programs/backend/src/utils/constant.rs programs/backend/src/utils/state.rs
 
 programs/backend/src/lib.rs:
+programs/backend/src/utils/constant.rs:
+programs/backend/src/utils/state.rs:
