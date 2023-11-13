7481e1808441ee3bbf454b3057fe0a77cf13670a
Lirae Que Data <liraedata59@gmail.com>
2023-11-13 15:59:57 +0800

    Add new constants and state files

--- backend/programs/backend/src/lib.rs
+++ backend/programs/backend/src/lib.rs
@@ -1,15 +1,4 @@
-use anchor_lang::prelude::*;
+
+
 
 declare_id!("92TRmey1biFtJMR8oMCMd2oZQmfiAF5CR3zvTQPixLCp");
-
-#[program]
-pub mod backend {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize {}
