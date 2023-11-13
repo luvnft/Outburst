28443b2e1d4d6f7465ad5f458295fbf6bfdeebe1
Lirae Que Data <liraedata59@gmail.com>
2023-11-13 16:36:23 +0800

    Update create post method

--- backend/programs/backend/src/lib.rs
+++ backend/programs/backend/src/lib.rs
@@ -27,8 +27,26 @@ pub mod main {
         Ok(())
     }
 
-    pub fn create_post(){
+    pub fn create_post(ctx : Context<CreatePost>, title : String, content : String) -> Result<()> {
+        let post_account = &mut ctx.accounts.post_account;
+        let user_account = &mut ctx.accounts.user_account;
+        let authority = &mut ctx.accounts.authority;
 
+        post_account.id = user_account.last_post_id;
+        post_account.title = title;
+        post_account.content = content;
+        post_account.user = user_account.key();
+        post_account.authority = authority.key();
+
+        user_account.last_post_id = user_account.last_post_id 
+            .checked_add(1)
+            .unwrap();
+
+        user_account.post_count = user_account.post_count
+            .checked_add(1)
+            .unwrap();
+
+        Ok(())
     }
 }
 
