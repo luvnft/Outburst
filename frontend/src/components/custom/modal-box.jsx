f9384c18464ffafcb21a8bd675850fd8f9fa9325
Lirae Que Data <liraedata59@gmail.com>
2023-11-13 21:44:20 +0800

    Add create post modal box

--- frontend/src/components/custom/modal-box.jsx
+++ frontend/src/components/custom/modal-box.jsx
@@ -1,6 +1,26 @@
-export const ModalBox = () => {
-    return (
-        <>
-        </>
-    );
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "../ui/card";
+
+export const ModalBox = ({ isVisible, onExit, children }) => {
+  return (
+    <>
+      {isVisible ? (
+        <section
+          className="fixed top-0 left-0 z-20 bg-[#00000059] w-screen h-screen flex items-center justify-center backdrop-blur-sm"
+          onClick={onExit}
+        >
+          <div className="absolute top-40w w-96 ">
+            <Card>
+                {children}
+            </Card>
+          </div>
+        </section>
+      ) : null}
+    </>
+  );
 };
--- frontend/src/pages/Dashboard/Feed/index.jsx
+++ frontend/src/pages/Dashboard/Feed/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -5,43 +6,148 @@ import {
   CardHeader,
   CardTitle,
 } from "../../../components/ui/card";
+import { Input } from "../../../components/ui/input";
+import { Label } from "../../../components/ui/label";
 import { Button } from "../../../components/ui/button";
 import { FcAddImage, FcLike } from "react-icons/fc";
+import { RxCross1 } from "react-icons/rx";
 import { PostCard } from "../../../components/custom/post-card";
+import { ModalBox } from "../../../components/custom/modal-box";
+import { usePost } from "../../../hooks";
 
 const Feed = () => {
+  const [isShowAddPost, setShowAddPost] = useState(false);
+  const [isShowSoonPost, setShowSoonPost] = useState(false);
+  const [
+    isLoading,
+    { errors, values, touched, handleSubmit, handleBlur, handleChange },
+  ] = usePost();
+
   return (
     <section className="mx-auto sm:container p-4 md:p-6 flex">
+
+      {/* Modal Box for Creating Post */}
+      <ModalBox isVisible={isShowAddPost} onExit={() => setShowAddPost(false)}>
+        <CardHeader>
+          <div className="font-medium flex items-center justify-between">
+            <p className="text-lg font-medium">Share Something...</p>
+            <RxCross1
+              size={32}
+              className="inline-block hover:bg-zinc-300 rounded-lg p-2"
+              onClick={() => setShowAddPost(false)}
+            />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <Label htmlFor="title">{"What are you feeling right now?"}</Label>
+              <Input
+                name="title"
+                id="title"
+                values={values.title}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+                className="bg-zinc-100"
+              />
+              <span className="text-red-500">
+                {errors.title && touched.title && errors.title}
+              </span>
+            </div>
+            <div className="mb-8">
+              <Label htmlFor="content">{"We're here to listen..."}</Label>
+              <Input
+                name="content"
+                id="content"
+                values={values.content}
+                handleBlur={handleBlur}
+                handleChange={handleChange}
+              />
+              <span className="text-red-500">
+                {errors.content && touched.content && errors.content}
+              </span>
+            </div>
+            <Button
+              type="submit"
+              className="bg-rose-500 h-12 w-full rounded-full md:text-base hover:bg-rose-600"
+            >
+              {isLoading ? (
+                <>
+                  <div className="w-6 h-6 rounded-full border-2 border-white border-t-transparent animate-spin inline-block mr-2"></div>
+                  Loading...
+                </>
+              ) : "Submit"}
+            </Button>
+          </form>
+        </CardContent>
+      </ModalBox>
+
+      {/* Modal Box for Uploading Pictures */}
+      <ModalBox
+        isVisible={isShowSoonPost}
+        onExit={() => setShowSoonPost(false)}
+      >
+        <CardHeader>
+          <div className="font-medium flex items-center justify-between">
+            <p className="text-lg">Feature is comming soon</p>
+            <RxCross1
+              size={32}
+              className="inline-block hover:bg-zinc-300 rounded-lg p-2"
+              onClick={() => setShowSoonPost(false)}
+            />
+          </div>
+        </CardHeader>
+      </ModalBox>
+
       <section className="w-1/2 hidden sm:block p-4">
+        
+        {/* Still thinking */}
         <Card></Card>
       </section>
-      <section className="w-full p-4">  
+      <section className="w-full p-4">
+        {/* Adding Blog Post here */}
         <Card className="mb-3">
           <CardHeader>
             <div className="flex items-center gap-3">
               <CardTitle>
                 <div className="h-10 w-10 bg-rose-500 rounded-full"></div>
               </CardTitle>
-              <CardDescription className="border bg-zinc-50 w-full p-2 md:px-5 rounded-full md:text-base">
+              <CardDescription
+                className="border bg-zinc-50 w-full p-2 md:px-5 rounded-full md:text-base cursor-pointer"
+                onClick={() => setShowAddPost(true)}
+              >
                 {"What's on your mind?"}
               </CardDescription>
             </div>
           </CardHeader>
           <CardContent className="flex gap-2">
-            <Button className="hover:bg-zinc-100 md:text-base rounded-full flex-grow bg-white shadow-none text-black border">
-              <FcAddImage size={24} className="mr-2" />Images
+            <Button
+              className="hover:bg-zinc-100 md:text-base rounded-full flex-grow bg-white shadow-none text-black border"
+              onClick={() => setShowSoonPost(true)}
+            >
+              <FcAddImage size={24} className="mr-2" />
+              Images
             </Button>
-            <Button className="hover:bg-zinc-100 md:text-base rounded-full flex-grow bg-white shadow-none text-black border">
-              <FcLike size={24} className="mr-2" />Feelings
+            <Button
+              className="hover:bg-zinc-100 md:text-base rounded-full flex-grow bg-white shadow-none text-black border"
+              onClick={() => setShowAddPost(true)}
+            >
+              <FcLike size={24} className="mr-2" />
+              Feelings
             </Button>
           </CardContent>
         </Card>
 
-        <PostCard name="Lirae Que Data" body="Hello world my name is lirae Hello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is lirae" title="sad"/>
-       
-       
+        {/* List of Blog Post */}
+        <PostCard
+          name="Lirae Que Data"
+          body="Hello world my name is lirae Hello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is liraeHello world my name is lirae"
+          title="sad"
+        />
       </section>
       <section className="w-1/2 hidden lg:block p-4">
+
+        {/* GitHub Owner here */}
         <Card></Card>
       </section>
     </section>
