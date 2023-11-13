44f87eb91bf278b47fb24fb9b42647496d17b66f
Lirae Que Data <liraedata59@gmail.com>
2023-11-13 16:17:12 +0800

    Add new init user struct

--- backend/programs/backend/src/lib.rs
+++ backend/programs/backend/src/lib.rs
@@ -5,8 +5,8 @@ pub mod utils {
     pub mod state;
 }
 
-use crate::utils::*;
-use crate::utils::*;
+use crate::utils::constant::*;
+use crate::utils::state::*;
 
 declare_id!("92TRmey1biFtJMR8oMCMd2oZQmfiAF5CR3zvTQPixLCp");
 
@@ -19,6 +19,23 @@ pub mod main {
     }
 
     pub fn create_post(){
-        
+
     }
+}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct InitUser<'info>{
+    #[account(
+        init, 
+        seeds = [USER_SEED, authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 2312 + 8
+    )]
+    pub user_account: Account<'info, User>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub systemm_program: Program<'info, System>
 }
