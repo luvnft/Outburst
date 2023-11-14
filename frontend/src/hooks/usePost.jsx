fceb4fed6cd2c0bdfa2d1bd0ae7144ba8d3ff16f
Lirae Que Data <liraedata59@gmail.com>
2023-11-14 11:40:15 +0800

    Add formik and yup to usePost hook

--- frontend/src/hooks/usePost.jsx
+++ frontend/src/hooks/usePost.jsx
@@ -2,31 +2,27 @@ import { useState } from "react";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 
-
 const usePost = () => {
-    const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-    const formik = useFormik({
-        initialValues : {
-            title : "",
-            content : ""
-        }, 
-        onSubmit : (values) => {
-            setLoading(true);
-            setTimeout(() => {
-                
-                
-                setLoading(false);
-            }, 1000);
-        },
-        validationSchema : Yup.object({
-            title : Yup.string()
-                .min(2, "Title must at least be 2 characters long.")
-                .required("Title is required."),
-            content : Yup.string().required("Content is required.")
-        })
-    })
-    return [isLoading, formik];
-}
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      content: "",
+    },
+    onSubmit: (values) => {
+      setLoading(true);
+      console.log(values);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    },
+    validationSchema: Yup.object({
+    //   title: Yup.string().required("It's okay, just vent it."),
+      content: Yup.string().required("Just vent on it."),
+    }),
+  });
+  return [isLoading, formik];
+};
 
 export default usePost;
