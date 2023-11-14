6560a7bf82ca242cef7a298d0a8547d53934cc78
Lirae Que Data <liraedata59@gmail.com>
2023-11-14 23:18:08 +0800

    Add new usePhantom hook

---
+++ frontend/src/hooks/usePhantom.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { PhantomWalletName } from "@solana/wallet-adapter-wallets";
+import { useBlog } from "../context/Blog";
+
+const usePhantom = () => {
+  const [connecting, setConnecting] = useState(false);
+  const { connected, select, publicKey } = useWallet();
+  const { user, disconnectWallet, posts } = useBlog();
+
+  const onConnect = () => {
+    setConnecting(true);
+    select(PhantomWalletName);
+  };
+
+  useEffect(() => {
+    if (user) {
+      setConnecting(false);
+    }
+  }, [user]);
+
+  return {
+    connecting,
+    connected,
+    user,
+    disconnectWallet,
+    posts,
+    publicKey,
+    onConnect,
+  };
+};
+
+export default usePhantom;
