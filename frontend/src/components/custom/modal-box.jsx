832cf6cbe5be7aeac4824c979c5e1498831fd66f
Lirae Que Data <liraedata59@gmail.com>
2023-11-15 10:35:17 +0800

    Add show animation

--- frontend/src/components/custom/modal-box.jsx
+++ frontend/src/components/custom/modal-box.jsx
@@ -1,4 +1,5 @@
 import { Card } from "../ui/card";
+import { motion } from "framer-motion";
 
 export const ModalBox = ({ isVisible, onExit, children }) => {
   const handleSectionClick = (e) => {
@@ -9,12 +10,15 @@ export const ModalBox = ({ isVisible, onExit, children }) => {
     <>
       {isVisible ? (
         <section
-          className="fixed top-0 left-0 z-50 bg-[#00000060] w-screen h-screen flex items-center justify-center backdrop-blur-sm"
+          className="fixed top-0 left-0 z-20 bg-[#00000060] w-screen h-screen flex items-center justify-center backdrop-blur-sm"
           onClick={onExit}
         >
-          <div className="absolute top-32 p-4 sm:p-0 h-full w-full max-w-[28rem]">
+          <motion.div 
+            initial={{y: -10}}
+            animate={{y:0}}
+            className="absolute top-32 p-4 sm:p-0 h-full w-full max-w-[28rem]">
             <Card onClick={handleSectionClick}>{children}</Card>
-          </div>
+          </motion.div>
         </section>
       ) : null}
     </>
